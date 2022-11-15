2493dd00ae20ac1b117842419a2e7c32069a3b6a
KatarzynaReluga <katarzynareluga@gmail.com>
2022-11-15 10:26:28 -0800

    Update

--- R/hte.R
+++ R/hte.R
@@ -144,6 +144,7 @@ hte <- function(type_hte = c("OR", "IPW", "NIPW", "AIPW"),
 #' @importFrom grf regression_forest
 #' @importFrom xgboost xgboost xgb.cv
 #' @importFrom mquantreg mquantreg
+#' @importFrom dplyr select %in%
 #'
 
 
@@ -217,8 +218,37 @@ estimate_hte.OR <- function(obj_hte,
 
 
 estimate_hte.IPW <- function(obj_hte,
-                             params_OR,
+                             params_p_score,
+                             params_impute_y,
                              ...) {
+
+  fit_p_score <- function(obj_hte, params_p_score) {
+
+    data_out_of_sample <- obj_hte$data_out_of_sample
+    names_out_of_sample <- names(data_out_of_sample)
+
+    data_sample <- obj_hte$data_sample
+
+
+    if ("y" %in% names_out_of_sample) {
+      #    y <- data_out_of_sample$y
+      data_out_of_sample_p_score <- select(data_out_of_sample, -y)
+      data_sample_p_score <- select(data_sample, -y)
+      data_p_score <- rbind(data_sample_p_score, data_out_of_sample_p_score)
+      obj_p_score <- list(data_p_score = data_p_score)
+    } else {
+      data_sample_p_score <- select(data_sample, -y)
+      data_p_score <- rbind(data_sample_p_score, data_out_of_sample_p_score)
+      obj_p_score <- list(data_p_score = data_p_score)
+    }
+
+    class(obj_p_score) <- params_p_score$method
+
+    ps_hat <- p_score(obj_p_score = obj_p_score,
+                      model_formula = params_p_score$model_formula,
+                      xgboost_params = params_p_score$xgboost_params,
+                      tune_RF = params_p_score$tune_RF)
+  }
 
 
 
